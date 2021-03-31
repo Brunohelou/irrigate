ab02c17c2693ac1e36f90c75371bfb73f542a71e
Bruno Helou <brunorhelou@gmail.com>
2021-03-30 22:27:35 -0300

    beneficios implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'irrigate';
   breakpoint:any;
+  breakpoint1:any;   
+  listHeight:any
 
   ngOnInit() {
     console.log(window.innerWidth);
     this.breakpoint = window.innerWidth/1000;
-    console.log(this.breakpoint);
+    this.breakpoint1 = window.innerWidth/600;
+
+    if(window.innerWidth <= 600){
+    this.listHeight = 300;
+  }else{
+    this.listHeight = 500;
+  }
+    console.log(window.innerWidth);
 }
 
 onResize(event:any) {
   this.breakpoint = window.innerWidth/900;
+  this.breakpoint1 = window.innerWidth/600;
+  if(window.innerWidth <= 600){
+    this.listHeight = 300;
+  }else{
+    this.listHeight = 500;
+  }
 }
 printed(name:string, phone: string, email: string, help: string) {
 
